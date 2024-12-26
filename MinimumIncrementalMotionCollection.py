8a38832a33196a246e1360388403b89b6648e6ba
tbates <tbates@aerotech.com>
2024-12-26 09:54:52 -0500

    Fixed issue applying probe sensitivity.

--- MinimumIncrementalMotionCollection.py
+++ MinimumIncrementalMotionCollection.py
@@ -220,8 +220,8 @@ class incremental_step(a1data.a1data):
         if self.units != 'deg':
             #If analog data is chosen, must be multiplied by the sensitivity        
             if self.probe_axis != 'None':
-                for axis in self.probe_axis:
-                    self.ai0 = [e * self.sens for e in self.ai0]
+                #for axis in self.probe_axis:
+                self.ai0 = [e * self.sens for e in self.ai0]
         
         if self.probe_axis != 'None':
             #Checks to see if probe direction sense matches the encoder and flips the sign if not
